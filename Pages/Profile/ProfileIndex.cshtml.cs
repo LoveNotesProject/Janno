8b039ac2e182018c760274e786230a4287c5bcdb
MantillaBigfoot <chris.gym@web.de>
2020-03-09 16:24:31 +0100

    Added ProfileIndex.cshtml, ProfileSettingIndex.cshtml Added material checkbox component

---
+++ Pages/Profile/ProfileIndex.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Janno.Data.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+
+namespace Janno.Pages.Profile {
+
+  public class ProfileIndexModel : PageModel {
+
+    private UserContext UserContext;
+    private UserManager UserManager;
+
+    [BindProperty]
+    public User User { get; set; }
+
+    public ProfileIndexModel(UserContext userContext, UserManager userManager) {
+      this.UserContext = userContext;
+      this.UserManager = userManager;
+    }
+
+    public async Task<IActionResult> OnGetAsync() {
+      this.User = await this.UserManager.GetUserAsync(HttpContext.User);
+      return Page();
+    }
+    
+    public async Task<PartialViewResult> OnGetProfileInterestViewAsync() {
+      this.User = await this.UserManager.GetUserAsync(HttpContext.User);
+      Console.WriteLine("this user id : " + this.User.Id);
+      this.ViewData["User"] = this.User;
+      return new PartialViewResult {
+        ViewName = "/Pages/Profile/Partial/ProfileInterestPartial.cshtml",
+        ViewData = this.ViewData
+      };
+    }
+    
+    public async Task<PartialViewResult> OnGetProfileSettingInterestViewAsync() {
+      this.User = await this.UserManager.GetUserAsync(HttpContext.User);
+      Console.WriteLine("this user id : " + this.User.Id);
+      this.ViewData["User"] = this.User;
+      return new PartialViewResult {
+        ViewName = "/Pages/Profile/Setting/Partial/ProfileSettingInterestPartial.cshtml",
+        ViewData = this.ViewData
+      };
+    }
+
+    public async Task<PartialViewResult> OnGetProfileSportViewAsync() {
+      this.User = await this.UserManager.GetUserAsync(HttpContext.User);
+      this.ViewData["User"] = this.User;
+      return new PartialViewResult {
+        ViewName = "/Pages/Profile/Partial/ProfileSportPartial.cshtml",
+        ViewData = this.ViewData
+      };
+    }
+    
+    
+
+  }
+
+}
---
+++ Pages/Profile/Setting/ProfileSettingIndex.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using Janno.Data.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Janno.Pages.Profile.Setting {
+
+  public class ProfileSettingIndex : PageModel {
+
+    private UserContext UserContext;
+    private UserManager UserManager;
+
+    [BindProperty]
+    public User User { get; set; }
+
+    public ProfileSettingIndex(UserContext userContext, UserManager userManager) {
+      this.UserContext = userContext;
+      this.UserManager = userManager;
+    }
+
+    public async Task<IActionResult> OnGetAsync() {
+      this.User = await this.UserManager.GetUserAsync(HttpContext.User);
+      return Page();
+    }
+
+    public async Task<PartialViewResult> OnGetProfileSettingUniversalViewAsync() {
+      this.User = await this.UserManager.GetUserAsync(HttpContext.User);
+      this.ViewData["User"] = this.User;
+      
+      return new PartialViewResult {
+        ViewName = "/Pages/Profile/Setting/Partial/ProfileSettingUniversalPartial.cshtml",
+        ViewData = this.ViewData
+      };
+    }
+
+    public async Task<PartialViewResult> OnGetProfileSettingInterestViewAsync() {
+      this.User = await this.UserManager.GetUserAsync(HttpContext.User);
+      this.ViewData["User"] = this.User;
+      
+      return new PartialViewResult {
+        ViewName = "/Pages/Profile/Setting/Partial/ProfileSettingInterestPartial.cshtml",
+        ViewData = this.ViewData
+      };
+    }
+
+    public async Task<PartialViewResult> OnGetProfileSettingPictureViewAsync() {
+      // ups, bei jeden get/post request muss der user neu geladen werden
+      this.User = await this.UserManager.GetUserAsync(HttpContext.User);
+
+      // hier ist der fehler
+      // du sagst hier, bitte lade die ProfileSettingPicturePartial.cshtml Datei -> OKE, aber mit welchen Daten?
+      // ViewDate is leer
+
+      this.ViewData["User"] = this.User; // wir packen user in die viewdate rein
+
+      return new PartialViewResult {
+        ViewName = "/Pages/Profile/Setting/Partial/ProfileSettingPicturePartial.cshtml",
+        ViewData = this.ViewData // leer :(
+      };
+    }
+
+  }
+
+}
